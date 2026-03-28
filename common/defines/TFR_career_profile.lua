ff75df65dd5feeb235829bfee2b040d7bc96eeff
stackallocator <me@stackallocator.dev>
2026-03-28 19:18:16 +0100

    init: initial commit

---
+++ common/defines/TFR_career_profile.lua
@@ -0,0 +1,7 @@
+NDefines_CareerProfile = {
+	NCareerProfile = {
+		-- Mod defines
+		MOD_STATISTICS_GROUP = "the_fire_rises", -- Can be set by mods to collect statistics in a separate data set for the mod. Will also be used as the display name if MOD_STATISTICS_GROUP_NAME is not set.
+		MOD_STATISTICS_GROUP_NAME = "the_fire_rises", -- Can be set by mods as a display name for the mod's statistics data set. It can be a localized.
+	},
+}
---
+++ descriptor.mod
@@ -0,0 +1,8 @@
+version="0.0.1"
+tags={
+	"Map"
+	"Gameplay"
+	"Ideologies"
+}
+name="at_tfr"
+supported_version="1.17.5.2"
